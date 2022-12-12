693cd276758495b2c27ff9753ca66daa5c617f89
kleitonrufino <kleiton.arufino@gmail.com>
2022-12-12 13:16:34 -0300

    estrutura inicial projeto: pom, properties

---
+++ src/main/java/br/com/Startup.java
@@ -0,0 +1,31 @@
+
+package br.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+public class Startup extends SpringBootServletInitializer {
+
+	
+	/*
+	 * 
+	 * http://localhost:8080/swagger-ui/index.htm
+	 * http://localhost:8080/h2-console
+	 * 
+	 * */
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Startup.class);
+	}
+}
